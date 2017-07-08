58f448ebfeea4523487e410f203a2ff60461422b
Zoe Ames <zoeames@gmail.com>
2017-07-07 23:30:53 -0700

    added weekly plan

--- javascripts/MainCtrl.js
+++ javascripts/MainCtrl.js
@@ -2,6 +2,15 @@
 
 app.controller("MainCtrl", function($scope, MainFactory){
 	$scope.assignments = [];
+	$scope.thisMonday = {};
+	$scope.thisTuesday = {};
+	$scope.thisStudygroup = {};
+	$scope.thisSaturday = {};
+	$scope.nextMonday = {};
+	$scope.nextTuesday = {};
+	$scope.nextStudygroup = {};
+	$scope.nextSaturday = {};
+
 	MainFactory.getAssignmentList().then(function(fbAssignments){
 		fbAssignments.sort(function(a,b){ 
   		return new Date(b.dueDate) - new Date(a.dueDate);
@@ -10,11 +19,41 @@ app.controller("MainCtrl", function($scope, MainFactory){
 	})
 
 	MainFactory.getThisWeek().then(function(planz){
-		console.log("thisWeek ", planz);
+		planz.forEach((plan) => {
+			switch (plan.id) {
+			  case 'monday':
+			    $scope.thisMonday = plan;
+			  	break;
+			  case 'saturday':
+			    $scope.thisSaturday = plan;
+			  	break;
+			  case 'studygroup':
+			    $scope.thisStudygroup = plan;
+			  	break;
+			  case 'tuesday':
+			    $scope.thisTuesday = plan;
+			    break;
+			};
+		});
 	});
 
 	MainFactory.getNextWeek().then(function(planz){
-		console.log("nextWeek ", planz);
+		planz.forEach((plan) => {
+			switch (plan.id) {
+			  case 'monday':
+			    $scope.nextMonday = plan;
+			  	break;
+			  case 'saturday':
+			    $scope.nextSaturday = plan;
+			  	break;
+			  case 'studygroup':
+			    $scope.nextStudygroup = plan;
+			  	break;
+			  case 'tuesday':
+			    $scope.nextTuesday = plan;
+			    break;
+			};
+		});
 	});
 
 	$scope.objectToArray= function(arr) {  
