ff920c6fdd38afbbffdcb369b6af9c3f4d9c29da
Zoe Ames <zoeames@gmail.com>
2018-01-28 20:00:06 -0600

    treehouse points coming in

--- javascripts/services/StudentService.js
+++ javascripts/services/StudentService.js
@@ -1,23 +1,32 @@
-app.service("StudentService", function($q, $http, FIREBASE_CONFIG) {
+app.service("StudentService", function ($q, $http, FIREBASE_CONFIG) {
 
-  var getStudentList = function() {
+  var getStudentList = function () {
     let students = [];
-    return $q(function(resolve, reject) {
+    return $q(function (resolve, reject) {
       $http.get(`${FIREBASE_CONFIG.databaseURL}/students.json`)
-        .then(function(studentObject) {
+        .then(function (studentObject) {
           var studentCollection = studentObject.data;
-          Object.keys(studentCollection).forEach(function(key) {
+          Object.keys(studentCollection).forEach(function (key) {
             studentCollection[key].id = key;
             students.push(studentCollection[key]);
           });
           resolve(students);
         })
-        .catch(function(error) {
+        .catch(function (error) {
           reject(error);
         });
     });
   };
 
-  return { getStudentList: getStudentList };
+  var getTreehouseProfilePoints = function (url) {
+    return $http.get(`${url}.json`)
+      .then(function (treehouseProfile) {
+        return treehouseProfile.data.points.total;
+      })
+      .catch(function (error) {
+        return errror;
+      });
+  };
 
+  return { getStudentList: getStudentList, getTreehouseProfilePoints: getTreehouseProfilePoints };
 });
