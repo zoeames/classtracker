d7bd0d5c5bb211c925ce6150300e403594635abd
Zoe Ames <zoeames@gmail.com>
2018-02-18 20:54:30 -0600

    added auth/logout buttons to navbar

--- src/app/components/navbar/navbar.controller.js
+++ src/app/components/navbar/navbar.controller.js
@@ -4,6 +4,14 @@ class NavbarController {
 
     this.testData = 'Navbar';
   }
+
+  auth(){
+    console.log('you hit auth');
+  }
+
+  logout(){
+    console.log('you hit logout');
+  }
 }
 
 export default NavbarController;
