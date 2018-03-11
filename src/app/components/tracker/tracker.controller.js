e0b3e6a1d7cee749d4bd0b564be6bbd24f7d2f24
Zoe Ames <zoeames@gmail.com>
2018-03-11 18:05:41 -0500

    tracker shows links to students pages

--- src/app/components/tracker/tracker.controller.js
+++ src/app/components/tracker/tracker.controller.js
@@ -1,28 +1,31 @@
 class TrackerController {
-  constructor(authService, assignmentService, submitAssignmentService) {
+  constructor(authService, assignmentService, studentService, submitAssignmentService) {
     "ngInject";
 
     this.assignmentService = assignmentService;
     this.submitAssignmentService = submitAssignmentService;
+    this.studentService = studentService;
     this.authService = authService;
+    
     this.assignments = [];
     this.uid = this.authService.getCurrentUid();
+    this.students = [];
   }
 
   $onInit() {
-    this.getGithubAssignments();
+    this.getStudentList();
   }
-
-  getGithubAssignments() {
-    this.assignmentService.getGithubAssignmentList().then(fbAssignments => {
-      
-      this.submitAssignmentService.getSubmitAssignmentsByUid(this.uid).then((myAssignments) =>{
-        let combinedAssignments = this.submitAssignmentService.smashLists(fbAssignments, myAssignments);
-        combinedAssignments.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
-        this.assignments = combinedAssignments;
-        console.log("assignments", this.assignments)
+  
+  getStudentList() {
+    this.studentService
+      .getStudentList()
+      .then(fbStudents => {
+        this.students = fbStudents;
+        console.log("students", this.students);
       })
-    });
+      .catch(err => {
+        console.error("error in students", err);
+      });
   }
 }
 
