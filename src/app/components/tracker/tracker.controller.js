803e9331af6412ee5051b73e25ddfbfaf38ddbc6
Zoe Ames <zoeames@gmail.com>
2018-03-16 20:57:13 -0500

    student names slanted

--- src/app/components/tracker/tracker.controller.js
+++ src/app/components/tracker/tracker.controller.js
@@ -1,21 +1,29 @@
 class TrackerController {
-  constructor(authService, assignmentService, studentService, submitAssignmentService) {
+  constructor(
+    authService,
+    assignmentService,
+    studentService,
+    submitAssignmentService
+  ) {
     "ngInject";
 
     this.assignmentService = assignmentService;
     this.submitAssignmentService = submitAssignmentService;
     this.studentService = studentService;
     this.authService = authService;
-    
+
     this.assignments = [];
     this.uid = this.authService.getCurrentUid();
     this.students = [];
+    this.assignments = [];
   }
 
   $onInit() {
     this.getStudentList();
+    this.getAssignmentList();
+    
   }
-  
+
   getStudentList() {
     this.studentService
       .getStudentList()
@@ -25,6 +33,18 @@ class TrackerController {
       })
       .catch(err => {
         console.error("error in students", err);
+      });
+  }
+
+  getAssignmentList() {
+    this.assignmentService
+      .getGithubAssignmentList()
+      .then(fbAssignments => {
+        this.assignments = fbAssignments;
+        console.log("assignments", this.assignments);
+      })
+      .catch(err => {
+        console.error("error in assignments", err);
       });
   }
 }
