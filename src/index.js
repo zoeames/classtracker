5b5e08af7c78a9ebc2440a3156c274856e695275
Zoe Ames <zoeames@gmail.com>
2018-08-10 21:51:02 -0500

    added reactstrap, navbar converted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
+
 import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
 
