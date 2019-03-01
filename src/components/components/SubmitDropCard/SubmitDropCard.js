0f055cb0d6bc1ae3ba8ce5b6dda64e60a7a05db6
Zoe Ames <zoe.ames@ingramcontent.com>
2019-03-01 15:44:45 -0600

    backlog assignments look good

--- src/components/components/SubmitDropCard/SubmitDropCard.js
+++ src/components/components/SubmitDropCard/SubmitDropCard.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 import './SubmitDropCard.scss';
+import assignmentRequests from '../../../helpers/data/assignmentRequests';
 
 class SubmitDropCard extends React.Component {
   static propTypes = {
@@ -17,14 +19,21 @@ class SubmitDropCard extends React.Component {
     return (
       <Draggable key={itemId} draggableId={itemId} index={index}>
         {(provided, snapshot) => (
-          <div
+          <div className="card submit-drop-card"
             ref={provided.innerRef}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             style={{ border: snapshot.isDragging ? '5px solid greenyellow' : '1px solid grey', ...provided.draggableProps.style }}
-            className="submit-drop-card"
           >
-            {item.title}
+            <div className="card-header">
+              {item.title}
+            </div>
+            <div className="card-body">
+              <h5 class="card-title">Due Date: {moment(item.dueDate).format('LL')}</h5>
+              <div>Get the assignment <a href={item.URL} target="_blank" rel="noopener noreferrer">HERE</a></div>
+              <div>Topic: {item.topic}</div>
+              <div>Notes: {item.notes}</div>
+            </div>
           </div>
         )}
       </Draggable>
