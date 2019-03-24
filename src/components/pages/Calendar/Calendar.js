dfc45665566b9612cac6a2fffd2d44a84d97ba37
Zoe Ames <zoeames@gmail.com>
2019-03-24 15:00:26 -0500

    week selection works

--- src/components/pages/Calendar/Calendar.js
+++ src/components/pages/Calendar/Calendar.js
@@ -7,7 +7,13 @@ import calRequests from '../../../helpers/data/calRequests';
 
 import './Calendar.scss';
 
-moment.locale('en-GB');
+// moment.locale('en-GB');
+moment.locale('ko', {
+  week: {
+    dow: 1,
+    doy: 1,
+  },
+});
 const localizer = BigCalendar.momentLocalizer(moment);
 
 class Calendar extends React.Component {
@@ -73,18 +79,17 @@ class Calendar extends React.Component {
   };
 
   selectDate = (slotInfo) => {
-    const startDate = new Date(slotInfo.end);
-    const startTime = startDate.getTime();
-    const endTime = startTime + 60 * 60 * 24 * 1000;
+    const startDate = new Date(slotInfo.start);
+    const endDate = new Date(slotInfo.start);
+    startDate.setHours(0, 0, 0);
+    endDate.setHours(23, 59, 0);
     const events = this.state.events.filter((event) => {
-      const eventStartDate = new Date(event.start);
-      const eventStartTime = eventStartDate.getTime();
-      const eventEndDate = new Date(event.end);
-      const eventEndTime = eventEndDate.getTime();
+      const eventStartTime = event.start.getTime();
+      const eventEndTime = event.end.getTime();
       return (
-        (startTime > eventStartTime && startTime < eventEndTime)
-        || (endTime > eventStartTime && endTime < eventEndTime)
-        || (eventStartTime >= startTime && eventStartTime <= endTime)
+        (startDate.getTime() > eventStartTime && startDate.getTime() < eventEndTime)
+        || (endDate.getTime() > eventStartTime && endDate.getTime() < eventEndTime)
+        || (eventStartTime >= startDate.getTime() && eventStartTime <= endDate.getTime())
       );
     });
     this.setState({ selectedEvents: events });
